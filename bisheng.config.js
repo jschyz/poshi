992da7c0c7f8812eeaf676654b9a0abc171d459b
huihui <skyalpha@126.com>
2017-03-28 23:31:50 +0800

    bisheng config

---
+++ bisheng.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  exclude: /should-be-ignore/,
+  theme: 'bisheng-theme-one',
+  themeConfig: {
+    home: '/',
+    sitename: 'One',
+    tagline: 'The one theme for bisheng',
+    github: 'https://github.com/benjycui/bisheng'
+  },
+  root: '/bisheng/',
+  port: 8000,
+};
